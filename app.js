163f2b7cba86b702ed53553c857f96120338c813
Obi Enechionyia <obienechionyia@gmail.com>
2022-10-18 00:01:49 +0200

    Add checkbox strikethrough, add undo button

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const container = document.querySelector('.reminder-container');
 const clearBtn = document.querySelector('.clear-btn');
 const clearChecked = document.querySelector('.clear-checked');
 const form = document.querySelector('.reminder-form');
+const undo = document.querySelector('.undo-btn');
 
 
 form.addEventListener('submit', addItem);
@@ -49,21 +50,22 @@ function addItem (e) {
         alert('please enter a nudge');
     }
 
-    const checkedBoxes = [];
+    let checkedBoxes = [];
 
     const checkboxes = document.querySelectorAll(".checkbox");
     checkboxes.forEach(function(box) {
         box.addEventListener('change', function() {
-            if (box.checked) {
+            if (box.checked && !checkedBoxes.includes(box)) {
                 box.parentElement.parentElement.children[0].classList.add('strike');
                 checkedBoxes.push(box);
             } else {
                 box.parentElement.parentElement.children[0].classList.remove('strike');
+                checkedBoxes = checkedBoxes.filter(function(item) {
+                    return item !== box;
+                })
             }
         })
     })
-
-    console.log(checkedBoxes);
 }
 
 function clearItems () {
@@ -77,7 +79,11 @@ function deleteItem (e) {
     if (reminderList.children.length === 0) {
         container.classList.remove('show-container');
     }
-    console.log(element);
+    undo.classList.add('show-item');
+    undo.addEventListener('click', function () {
+        container.classList.add('show-container');
+        reminderList.appendChild(element);
+    })
 }
 
 function editItem (e) {
@@ -86,3 +92,16 @@ function editItem (e) {
     editFlag = true;
     submitBtn.textContent = "edit";
 }
+
+// function showClearChecked (array) {
+//     if (array.length > 0) {
+//         clearChecked.classList.add('show-item');
+//     } else {
+//         clearChecked.classList.remove('show-item');
+//     }
+//     clearChecked.addEventListener('click', function () {
+//         array.forEach(function (check) {
+//             check.parentElement.parentElement.innerHTML = "";
+//         })
+//     })
+// }
